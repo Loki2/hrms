6b2184d84c3f2c3468f7c05b37805969ba6422c9
loki <stevloki@ymail.com>
2020-01-04 19:33:35 +0700

    ejs initial commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-if(process.env.NODE_ENV !== 'Production') {
-    require('datenv').load()
-}
+//require("dotenv").config();
+// if(process.env.NODE_ENV !== 'Production') {
+//     require('dotenv').load()
+// }
 const http = require('http');
 const app = require('./src/app');
 
--- src/app.js
+++ src/app.js
@@ -9,6 +9,34 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
+app.use(function(req, res, next){
+    res.status(404); 
+    if (req.accepts('ejs')) {// respond with html page
+      res.render('404', { url: req.url });
+      return;
+    } 
+    if (req.accepts('json')) {// respond with json
+      res.send({ error: 'Page Not found' });
+      return;
+    } 
+    res.type('txt').send('Not found');// default to plain-text. send()
+  });
+/*
+ **** All Routes is get from here
+ */
+const usersRoute = require('./routes/users.Route');
+
+
+
+
+
+
+/*
+ **** Use all Routes
+ */
+app.use('/users', usersRoute);
+
+
 /*
  **** Use error handling Routes
  */
